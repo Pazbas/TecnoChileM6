f949cab9be11cfc40ae71cefdfc0c9c71d333181
Pazbas <paz.bascunan.g@gmail.com>
2025-11-22 04:16:28 -0300

    Update API.js

--- src/services/API.js
+++ src/services/API.js
@@ -7,7 +7,7 @@ export const Products = {
           {id:2, name:"Mouse", price:25990, stock:10, img:"../src/assets/productos/mouse.jpg"},
           {id:3, name:"Teclado", price:45990, stock:8, img:"../src/assets/productos/teclado.jpg"},
           {id:4, name:"Monitor", price:199990, stock:6, img:"../src/assets/productos/monitor_gamer.jpg"},
-          {id:5, name:"Auriculares Gamer", price:79990, stock:7, img:"../src/assets/productos/auriculares_gamer.jpg"},
+          {id:5, name:"Auriculares Gamer", price:79990, stock:7, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/auriculares_gamer.jpg"},
           {id:6, name:"Nintendo Switch 2", price:349990, stock:4, img:"../src/assets/productos/nintendo_switch_2.jpg"},
           {id:7, name:"Mini Dron", price:459990, stock:9, img:"../src/assets/productos/mini_dron.jpg"},
           {id:8, name:"Impresora", price:119990, stock:12, img:"https://github.com/Pazbas/TecnoChileM6/blob/main/public/impresora.jpg"},
