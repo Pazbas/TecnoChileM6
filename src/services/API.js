fb02779b1695bdfb8b8af691b165b8425fa12a69
Pazbas <paz.bascunan.g@gmail.com>
2025-11-23 00:44:24 -0300

    Update API.js

--- src/services/API.js
+++ src/services/API.js
@@ -3,15 +3,15 @@ export const Products = {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve([
-          {id:1, name:"Notebook HP", price:550990, stock:5, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/notebook_hp_pavilion.jpg"},
-          {id:2, name:"Mouse", price:25990, stock:10, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/mouse.jpg"},
-          {id:3, name:"Teclado", price:45990, stock:8, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/teclado.jpg"},
-          {id:4, name:"Monitor", price:199990, stock:6, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/monitor_gamer.jpg"},
-          {id:5, name:"Auriculares Gamer", price:79990, stock:7, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/auriculares_gamer.jpg"},
-          {id:6, name:"Nintendo Switch 2", price:349990, stock:4, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/nintendo_switch_2.jpg"},
-          {id:7, name:"Mini Dron", price:459990, stock:9, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/mini_dron.jpg"},
-          {id:8, name:"Impresora", price:119990, stock:12, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/impresora.jpg"},
-          {id:9, name:"Roku Express", price:49990, stock:3, img:"https://raw.githubusercontent.com/Pazbas/TecnoChileM6/refs/heads/main/src/assets/productos/roku_express.jpg"},
+          {id:1, name:"Notebook HP", price:550990, stock:5, img:"/assets/productos/notebook_hp_pavilion.jpg"},
+          {id:2, name:"Mouse", price:25990, stock:10, img:"/assets/productos/mouse.jpg"},
+          {id:3, name:"Teclado", price:45990, stock:8, img:"/assets/productos/teclado.jpg"},
+          {id:4, name:"Monitor", price:199990, stock:6, img:"/assets/productos/monitor_gamer.jpg"},
+          {id:5, name:"Auriculares Gamer", price:79990, stock:7, img:"/assets/productos/auriculares_gamer.jpg"},
+          {id:6, name:"Nintendo Switch 2", price:349990, stock:4, img:"/assets/productos/nintendo_switch_2.jpg"},
+          {id:7, name:"Mini Dron", price:459990, stock:9, img:"/assets/productos/mini_dron.jpg"},
+          {id:8, name:"Impresora", price:119990, stock:12, img:"/assets/productos/impresora.jpg"},
+          {id:9, name:"Roku Express", price:49990, stock:3, img:"/assets/productos/roku_express.jpg"},
         ])
       }, 300);
     })
